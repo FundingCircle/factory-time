370018b7cf12c77270336110de9edf4a6cefc6dd
Aaron Probus <aprobus15@gmail.com>
2015-02-28 10:34:49 -0800

    Updates project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject factory_time "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject factory_time "0.1.0"
+  :description "A clojure repository inspired by factory_girl"
+  :url "https://github.com/FundingCircle/factory-time"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
