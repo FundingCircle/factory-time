f39ebe3562aad84b9f319e5127c9f0fb5b975e84
Aaron Probus <pair+aaron@fundingcircle.com>
2015-03-06 12:53:31 -0800

    Updates license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject factory-time "0.1.1"
   :description "A clojure repository inspired by factory_girl"
   :url "https://github.com/FundingCircle/factory-time"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "BSD 3-clause"
+            :url "http://opensource.org/licenses/BSD-3-Clause"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
   :profiles {:dev {:dependencies [[speclj "3.1.0"]]}}
   :plugins [[speclj "3.1.0"]]
