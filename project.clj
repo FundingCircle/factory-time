7c9e20e89426221536fd5ea1073583be73cce90a
Sasha Gerrand <sasha.gerrand@fundingcircle.com>
2016-04-25 16:26:55 +0100

    Updated dependencies

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/FundingCircle/factory-time"
   :license {:name "BSD 3-clause"
             :url "http://opensource.org/licenses/BSD-3-Clause"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[speclj "3.1.0"]]}}
-  :plugins [[speclj "3.1.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
+  :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
